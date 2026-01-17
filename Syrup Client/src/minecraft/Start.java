be9c713f8b5f290bf41c61876df6a2414dd7bd5f
Adin <malocic.adin@gmx.at>
2026-01-17 01:28:49 +0100

    Test Commit

--- Syrup Client/src/minecraft/Start.java
+++ Syrup Client/src/minecraft/Start.java
@@ -6,7 +6,8 @@ public class Start {
 	public static void main(String[] args) {
 		Main.main(concat(new String[] { "--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}" }, args));
 	}
-//test push
+
+	//test push
 	public static <T> T[] concat(T[] first, T[] second) {
 		T[] result = Arrays.copyOf(first, first.length + second.length);
 		System.arraycopy(second, 0, result, first.length, second.length);
